d3eaa407a3798efd873191a6f71ecde0cfa0ee44
79608 <mestkovskyyaroslavd@mail.ru>
2022-07-23 16:34:05 +0300

    4 коммит. - Название мода изменено на W40KCastleFight - Все упоминания о BreachMod удалены - Все наработки по СМ и ХСМ удалены - Найдены и восстановлены наработки по CF 2021 года Версия 1.0

---
+++ Data/scar/winconditions/Castle_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+exclusive = false,
+victory_condition = true,
+always_on = false,
+title = "Addon: Castle Wars WH40K",
+win_message = "",
+lose_message = "",
+description = "Neveroyatnaya Pizdilka"
+}
