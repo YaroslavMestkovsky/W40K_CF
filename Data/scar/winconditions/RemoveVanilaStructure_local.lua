0c581b2b58b9ef533e06cc19d9055c8ba7a927bd
Yaroslav <mestkovskyyaroslavd@mail.ru>
2023-09-11 20:43:35 +0300

    Правки + переосмысление. Скар для тестов. Новая вики.

---
+++ Data/scar/winconditions/RemoveVanilaStructure_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+exclusive = false,
+victory_condition = false,
+always_on = false,
+title = "Remove Vanila Structure",
+win_message = "",
+lose_message = "",
+description = "Remove Vanila Structure"
+}
